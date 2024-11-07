567c39b72be2309a5ed5a280dee3c6f6272c4230
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-11-07 10:16:08 +0100

    Steg 2

--- Näsan-1/Program.cs
+++ Näsan-1/Program.cs
@@ -1,6 +1,20 @@
-﻿if (6 > 3 || 6 == 3)
+﻿// Punkt 1
+if (6 > 3 || 6 == 3)
 {
     Console.WriteLine("Hello World!");
 }
+
+// Punkt 2
+Console.WriteLine("Please write your username");
+string answer = Console.ReadLine();
+
+if (answer == "noname")
+{
+    Console.WriteLine("Welcome!");
+}
+
+
+
+
 
 Console.ReadLine();
