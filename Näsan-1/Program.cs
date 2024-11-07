3a7b13c6ee73ddc12dadd61184ff9ea2dad3533f
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-11-07 10:20:03 +0100

    steg 3

--- Näsan-1/Program.cs
+++ Näsan-1/Program.cs
@@ -4,13 +4,19 @@ if (6 > 3 || 6 == 3)
     Console.WriteLine("Hello World!");
 }
 
-// Punkt 2
+// Punkt 2 och 3
 Console.WriteLine("Please write your username");
-string answer = Console.ReadLine();
+string answer1 = Console.ReadLine();
+Console.WriteLine("Please write your password");
+string answer2 = Console.ReadLine();
 
-if (answer == "noname")
+if (answer1 == "noname" && answer2 == "nopass")
 {
-    Console.WriteLine("Welcome!");
+    Console.WriteLine("Welcome");
+}
+else 
+{
+    Console.WriteLine("Wrong username or password");
 }
 
 
