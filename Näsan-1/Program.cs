36d0aaea41ad3c1af8a0651a28d3af3332addb00
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2024-11-07 10:29:14 +0100

    Steg 5

--- Näsan-1/Program.cs
+++ Näsan-1/Program.cs
@@ -4,11 +4,22 @@ if (6 > 3 || 6 == 3)
     Console.WriteLine("Hello World!");
 }
 
+// Punkt 4
+for (int i = 0; i < 32; i++)
+{
+    Console.WriteLine("Hello, World");
+}
+
 // Punkt 2 och 3
 Console.WriteLine("Please write your username");
 string answer1 = Console.ReadLine();
-Console.WriteLine("Please write your password");
-string answer2 = Console.ReadLine();
+
+string answer2 = ""; // Punkt 5
+while (answer2 != "nopass")
+{
+    Console.WriteLine("Please write your password");
+    answer2 = Console.ReadLine();
+}
 
 if (answer1 == "noname" && answer2 == "nopass")
 {
@@ -17,12 +28,6 @@ if (answer1 == "noname" && answer2 == "nopass")
 else 
 {
     Console.WriteLine("Wrong username or password");
-}
-
-// Punkt 4
-for (int i = 0; i < 32; i++)
-{
-    Console.WriteLine("Hello, World");
 }
 
 
